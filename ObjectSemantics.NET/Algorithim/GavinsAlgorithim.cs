5885bd686687d26f4d7e10dffe8dfef42c7af657
George Wainaina <georgewainaina18@gmail.com>
2022-08-10 04:31:51 +0300

    Completed Obj Enum Direct Access Loop

--- ObjectSemantics.NET/Algorithim/GavinsAlgorithim.cs
+++ ObjectSemantics.NET/Algorithim/GavinsAlgorithim.cs
@@ -1,8 +1,10 @@
 using ObjectSemantics.NET;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
 
 public static class GavinsAlgorithim
@@ -11,17 +13,49 @@ public static class GavinsAlgorithim
     public static string GenerateFromTemplate<T>(T record, TemplatedContent originTemplateContent, List<ObjectSemanticsKeyValue> parameterKeyValues = null) where T : new()
     {
         //To Prevent from Modifying 
-        TemplatedContent clonedTemplate = new TemplatedContent { Template = originTemplateContent.Template, ReplaceCodes = originTemplateContent.ReplaceCodes };
+        TemplatedContent clonedTemplate = new TemplatedContent { Template = originTemplateContent.Template, ReplaceCodes = originTemplateContent.ReplaceCodes, ReplaceObjLoopCodes = originTemplateContent.ReplaceObjLoopCodes };
         //Get Object's Properties
         List<ExtractedObjProperty> objProperties = GetObjectProperties(record, parameterKeyValues);
 
-        ReplaceObjectTemplateExecutables(clonedTemplate, objProperties);
+        #region Replace Obj Loop Attributes
+        foreach (ReplaceObjLoopCode objLoop in clonedTemplate.ReplaceObjLoopCodes)
+        {
+            //First Loop lets look at its target property Object
+            ExtractedObjProperty targetObj = objProperties.Where(x => x.IsEnumerableObject).FirstOrDefault(x => x.Name.ToUpper().Equals(objLoop.TargetObjectName.ToUpper()));
+            //Since this Object is of Type Enumerable
+            if (targetObj != null && targetObj.OriginalValue != null)
+            {
+                //Loop all data records inside target Property
+                string rowTemplate = objLoop.ObjLoopTemplate;
+                StringBuilder rowContentTemplater = new StringBuilder();
+                foreach (object loopRow in (IEnumerable)targetObj.OriginalValue)
+                {
+                    List<ExtractedObjProperty> rowRecordValues = GetObjPropertiesFromUnknown(loopRow);
+                    //Loop Through class template Loops
+                    string activeRow = rowTemplate;
+                    foreach (ReplaceCode objLoopCode in objLoop.ReplaceObjCodes)
+                    {
+                        ExtractedObjProperty objProperty = rowRecordValues.FirstOrDefault(x => x.Name.ToUpper().Equals(objLoopCode.TargetPropertyName.ToUpper()));
+                        if (objProperty != null)
+                            activeRow = ReplaceFirstOccurrence(activeRow, objLoopCode.ReplaceRef, GetValueFromPropertyFormatted(objProperty, objLoopCode.FormattingCommand));
+                        else
+                            activeRow = ReplaceFirstOccurrence(activeRow, objLoopCode.ReplaceRef, objLoopCode.ReplaceCommand);
+                    }
+                    //Append Record row
+                    rowContentTemplater.Append(activeRow);
+                }
 
-        return clonedTemplate.Template;
-    }
+                objLoop.ObjLoopTemplate = rowContentTemplater.ToString(); //Assign Auto Generated
+                //Replace the main Loop area
+                clonedTemplate.Template = ReplaceFirstOccurrence(clonedTemplate.Template, objLoop.ReplaceRef, objLoop.ObjLoopTemplate);
+            }
+            else
+                clonedTemplate.Template = ReplaceFirstOccurrence(clonedTemplate.Template, objLoop.ReplaceRef, string.Empty);
 
-    private static void ReplaceObjectTemplateExecutables(TemplatedContent clonedTemplate, List<ExtractedObjProperty> objProperties)
-    {
+        }
+        #endregion
+
+        #region Replace Direct Target Attributes
         foreach (ReplaceCode replaceCode in clonedTemplate.ReplaceCodes)
         {
             ExtractedObjProperty property = objProperties.FirstOrDefault(x => x.Name.ToUpper().Equals(replaceCode.TargetPropertyName.ToUpper()));
@@ -30,6 +64,9 @@ public static class GavinsAlgorithim
             else
                 clonedTemplate.Template = ReplaceFirstOccurrence(clonedTemplate.Template, replaceCode.ReplaceRef, replaceCode.ReplaceCommand);
         }
+        #endregion
+
+        return clonedTemplate.Template;
     }
 
     internal static TemplatedContent GenerateTemplateFromFile(string fileContent)
@@ -48,8 +85,32 @@ public static class GavinsAlgorithim
             //Replace Code Block
             string replaceCode = string.Format("REPLACE_LOOP_{0}", Guid.NewGuid().ToString().ToUpper());
             templatedContent.Template = Regex.Replace(templatedContent.Template, subBlock, replaceCode, RegexOptions.IgnoreCase);
+            //Loop Target Object Name
+            string targetObjName = regexLoopMatch.Value.Replace("{", string.Empty).Replace("}", string.Empty).Trim().Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
             //Obj
+            ReplaceObjLoopCode replaceObjLoopCode = new ReplaceObjLoopCode { ReplaceRef = replaceCode, TargetObjectName = targetObjName };
+            //Extra Loop Contents
+            Match subMatch = Regex.Match(subBlock, @"{{(.+?)}}", RegexOptions.IgnoreCase);
+            while (subMatch.Success)
+            {
+                //Reclean again
+                string actualCmdProperty = string.Format(@"{0}{1}{2}", "{{", subMatch.Groups[1].Value.Trim(), "}}");
+                //Skip Loop Block
+                if (!actualCmdProperty.StartsWith("{{for-each-start:") && !actualCmdProperty.StartsWith("{{for-each-end:"))
+                {
+                    //Replace Command Occurrence
+                    string replaceSubCode = string.Format("REPLACE_LOOP_RECORD_{0}", Guid.NewGuid().ToString().ToUpper());
+                    subBlock = ReplaceFirstOccurrence(subBlock, subMatch.Value, replaceSubCode);
+                    replaceObjLoopCode.ReplaceObjCodes.Add(new ReplaceCode { ReplaceCommand = actualCmdProperty, ReplaceRef = replaceSubCode });
+                }
+                else
+                    subBlock = ReplaceFirstOccurrence(subBlock, subMatch.Value, string.Empty);
+                //Proceed To Next
+                subMatch = subMatch.NextMatch();
+            }
 
+            replaceObjLoopCode.ObjLoopTemplate = Regex.Replace(subBlock, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline); //Assigning it here because we are modifying it above
+            templatedContent.ReplaceObjLoopCodes.Add(replaceObjLoopCode);
             //Move Next (Both)
             regexLoopMatch = regexLoopMatch.NextMatch();
             regexLoopEnd = regexLoopEnd.NextMatch();
@@ -105,6 +166,28 @@ public static class GavinsAlgorithim
                 extractedObjProperties.Add(new ExtractedObjProperty { Type = param.Type, Name = param.Key, OriginalValue = param.Value });
         return extractedObjProperties;
     }
+    private static List<ExtractedObjProperty> GetObjPropertiesFromUnknown(object value)
+    {
+        List<ExtractedObjProperty> list = new List<ExtractedObjProperty>();
+        if (value == null)
+            return list;
+        Type myType = value.GetType();
+        IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+        foreach (PropertyInfo prop in props)
+        {
+            try
+            {
+                list.Add(new ExtractedObjProperty
+                {
+                    Type = prop.PropertyType,
+                    Name = prop.Name,
+                    OriginalValue = value == null ? null : prop.GetValue(value)
+                });
+            }
+            catch { }
+        }
+        return list;
+    }
 
     private static string GetValueFromPropertyFormatted(ExtractedObjProperty p, string customFormattingValue)
     {
@@ -141,8 +224,9 @@ public class TemplatedContent
 public class ReplaceObjLoopCode
 {
     public string ReplaceRef { get; set; }
+    public string TargetObjectName { get; set; }
     public string ObjLoopTemplate { get; set; }
-    public List<ReplaceCode> ReplaceCodes { get; set; } = new List<ReplaceCode>();
+    public List<ReplaceCode> ReplaceObjCodes { get; set; } = new List<ReplaceCode>();
 }
 public class ReplaceCode
 {
