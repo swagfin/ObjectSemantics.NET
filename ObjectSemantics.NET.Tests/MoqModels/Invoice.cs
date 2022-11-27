cdb43f406dcc5755aeb54ff33e906b4c34a35f25
George Wainaina <georgewainaina18@gmail.com>
2022-11-27 18:28:51 +0300

    added units tests

---
+++ ObjectSemantics.NET.Tests/MoqModels/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ObjectSemantics.NET.Tests.MoqModels
+{
+    internal class Invoice
+    {
+        public int Id { get; set; }
+        public string RefNo { get; set; }
+        public string Narration { get; set; }
+        public double Amount { get; set; }
+        public DateTime InvoiceDate { get; set; }
+    }
+}
---
+++ ObjectSemantics.NET.Tests/MoqModels/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectSemantics.NET.Tests.MoqModels
+{
+    internal class Student
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string StudentName { get; set; }
+        public double Balance { get; set; }
+        public DateTime RegDate { get; set; } = DateTime.Now;
+        public List<Invoice> Invoices { get; set; } = new List<Invoice>();
+    }
+}
